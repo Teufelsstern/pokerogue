129cd9d0fdaa6127bfc7489dda009f71f39e841e
Teufelsstern <97199480+Teufelsstern@users.noreply.github.com>
2024-05-28 18:49:56 +0200

    Added option to skip dialogue

--- src/system/settings.ts
+++ src/system/settings.ts
@@ -30,7 +30,8 @@ export enum Setting {
   Gamepad_Support = "GAMEPAD_SUPPORT",
   Swap_A_and_B = "SWAP_A_B", // Swaps which gamepad button handles ACTION and CANCEL
   Touch_Controls = "TOUCH_CONTROLS",
-  Vibration = "VIBRATION"
+  Vibration = "VIBRATION",
+  Skip_After_Attack_Messages = "SKIP_MESSAGES"
 }
 
 export interface SettingOptions {
@@ -64,7 +65,8 @@ export const settingOptions: SettingOptions = {
   [Setting.Gamepad_Support]: ["Auto", "Disabled"],
   [Setting.Swap_A_and_B]: ["Enabled", "Disabled"],
   [Setting.Touch_Controls]: ["Auto", "Disabled"],
-  [Setting.Vibration]: ["Auto", "Disabled"]
+  [Setting.Vibration]: ["Auto", "Disabled"],
+  [Setting.Skip_After_Attack_Messages]: ["On", "Off]
 };
 
 export const settingDefaults: SettingDefaults = {
@@ -90,7 +92,8 @@ export const settingDefaults: SettingDefaults = {
   [Setting.Gamepad_Support]: 0,
   [Setting.Swap_A_and_B]: 1, // Set to 'Disabled' by default
   [Setting.Touch_Controls]: 0,
-  [Setting.Vibration]: 0
+  [Setting.Vibration]: 0,
+  [Setting.Skip_After_Attack_Messages]: 0
 };
 
 export const reloadSettings: Setting[] = [Setting.UI_Theme, Setting.Language, Setting.Sprite_Set];
@@ -126,6 +129,9 @@ export function setSetting(scene: BattleScene, setting: Setting, value: integer)
     break;
   case Setting.Enable_Retries:
     scene.enableRetries = settingOptions[setting][value] === "On";
+    break;
+  case Setting.Skip_After_Attack_Messages:
+    scene.Skip_After_Attack_Messages = settingOptions[setting][value] === "On";
     break;
   case Setting.Money_Format:
     switch (settingOptions[setting][value]) {
